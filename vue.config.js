f1a1722347e16d129b8f357b9393cedc5afee64f
Eric Mast <ericmast.personal@gmail.com>
2021-02-18 13:53:01 -0500

    updated config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/ParkingValetWebSite/'
-    : '/'
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/ParkingValetWebSite/public/' :
+        '/'
 }
